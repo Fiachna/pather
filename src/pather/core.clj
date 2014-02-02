12c5e5c1f502d0d6e0c1644368aa816cebc11eb1
Fiachna <fiachna.carter@gmail.com>
2014-02-02 15:20:09 +1100

    add window initialisation

--- src/pather/core.clj
+++ src/pather/core.clj
@@ -1,9 +1,11 @@
 (ns pather.core
   (:gen-class))
 
+(use 'pather.ui)
+
 (defn -main
   "Initialise the app, get the UI built and begin the main loop"
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (create-window))
--- src/pather/ui.clj
+++ src/pather/ui.clj
@@ -1,3 +1,18 @@
-(ns UI
-  :use [seesaw [core]]
-  (:use seesaw.graphics [seesaw.color :only [to-color]]))
+(ns pather.ui
+  (:use [seesaw.core]))
+
+(def window-id "pather-window")
+(def window-title "pather")
+(def window-size {:width 1280
+                  :height 960})
+
+(def main-window
+  (frame :id window-id
+            :title window-title
+            :width (:width window-size)
+            :height (:height window-size)))
+
+(defn create-window []
+  (show! main-window))
+
+(defn build-ui [])
